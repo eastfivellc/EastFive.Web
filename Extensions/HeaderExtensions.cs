edb1a581018cc9c677be98a396d4766c34baa206
Keith Holloway <keith@eastfive.com>
2016-03-24 16:53:18 -0500

    Added extensions for header manipulation and usage

---
+++ Extensions/HeaderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace BlackBarLabs.Web
+{
+    public static class HeaderExtensions
+    {
+        public static Guid GetUserIdFromAuthorizationHeader(this AuthenticationHeaderValue header)
+        {
+            try
+            {
+                var securityClientJwtString = header.ToString().Substring(7);
+                var securityClientJwt = new JwtSecurityToken(securityClientJwtString);
+                var claimsDict = securityClientJwt.Claims.ToDictionary(claim => claim.Type, claim => claim.Value);
+                return Guid.Parse(claimsDict[BlackBarLabs.Security.ClaimIds.Authorization]);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Problem getting user id from Authorization header");
+            }
+        }
+    }
+}
