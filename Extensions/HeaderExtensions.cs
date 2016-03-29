d036ef0a62d31de5f9bd292ad78db2e03d3e632f
joshcodes <josh@blackbarlabs.com>
2016-03-28 20:50:49 -0500

    made jwt parse more robust

--- Extensions/HeaderExtensions.cs
+++ Extensions/HeaderExtensions.cs
@@ -11,7 +11,10 @@ namespace BlackBarLabs.Web
         {
             try
             {
-                var securityClientJwtString = header.ToString().Substring(7);
+                var jwtStringPossibleBearer = header.ToString();
+                var securityClientJwtString = jwtStringPossibleBearer.ToLower().StartsWith("bearer ") ?
+                    jwtStringPossibleBearer.Substring(7) :
+                    jwtStringPossibleBearer;
                 var securityClientJwt = new JwtSecurityToken(securityClientJwtString);
                 var claimsDict = securityClientJwt.Claims.ToDictionary(claim => claim.Type, claim => claim.Value);
                 return Guid.Parse(claimsDict[BlackBarLabs.Security.ClaimIds.Authorization]);
