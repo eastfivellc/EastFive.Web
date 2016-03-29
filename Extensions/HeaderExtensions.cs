bbf488c18dc0f0c5d6ec528cf56fc78f4f80b463
Jeremy Randolph <randocodes@gmail.com>
2016-03-29 15:16:02 -0500

    Checking in Header Extensions

--- Extensions/HeaderExtensions.cs
+++ Extensions/HeaderExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 
 namespace BlackBarLabs.Web
 {
@@ -18,6 +20,25 @@ namespace BlackBarLabs.Web
                 var securityClientJwt = new JwtSecurityToken(securityClientJwtString);
                 var claimsDict = securityClientJwt.Claims.ToDictionary(claim => claim.Type, claim => claim.Value);
                 return Guid.Parse(claimsDict[BlackBarLabs.Security.ClaimIds.Authorization]);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Problem getting user id from Authorization header");
+            }
+        }
+
+
+        public static IEnumerable<Claim> GetClaimsFromAuthorizationHeader(this AuthenticationHeaderValue header)
+        {
+            try
+            {
+                var jwtStringPossibleBearer = header.ToString();
+                var securityClientJwtString = jwtStringPossibleBearer.ToLower().StartsWith("bearer ") ?
+                    jwtStringPossibleBearer.Substring(7) :
+                    jwtStringPossibleBearer;
+                var securityClientJwt = new JwtSecurityToken(securityClientJwtString);
+                var claimsDict = securityClientJwt.Claims.ToDictionary(claim => claim.Type, claim => claim.Value);
+                return claimsDict.Select(claim => new Claim(claim.Key, claim.Value)).ToList();
             }
             catch (Exception)
             {
