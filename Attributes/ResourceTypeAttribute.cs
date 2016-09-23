099b6228513e9a9f2effbfc75a7d0176808a73ac
Josh <josh@eastfive.com>
2016-09-23 10:26:14 -0500

    added new ResourceTypeAttribute for URNs and other Access / URN fun

---
+++ Attributes/ResourceTypeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BlackBarLabs.Web
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ResourceTypeAttribute : System.Attribute
+    {
+        public ResourceTypeAttribute()
+        {
+        }
+        
+        private string urn;
+        public string Urn
+        {
+            get
+            {
+                return this.urn;
+            }
+            set
+            {
+                urn = value;
+            }
+        }
+        
+    }
+}
---
+++ Extensions/UrnExtensions.cs
@@ -0,0 +1,37 @@
+﻿using BlackBarLabs.Core;
+using BlackBarLabs.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BlackBarLabs.Core.Collections;
+
+namespace BlackBarLabs.Web
+{
+    public static class UrnExtensions
+    {
+        public static ResourceQuery ParseUrnQuery(this Uri urn, Assembly assembly)
+        {
+            var models = assembly.GetTypes()
+                .Where(type => type.ContainsCustomAttribute<ResourceTypeAttribute>())
+                .ToArray();
+            return urn.ParseUrnQuery(models);
+        }
+
+        public static ResourceQuery ParseUrnQuery(this Uri urn, Type [] models)
+        {
+            var urlModelLookup = models
+                .Where(type => type.ContainsCustomAttribute<ResourceTypeAttribute>())
+                .Select(type =>
+                {
+                    var resourceTypeAttr = type.GetCustomAttribute<ResourceTypeAttribute>();
+                    return new KeyValuePair<string, Type>(resourceTypeAttr.Urn, type);
+                })
+                .ToDictionary();
+
+            return new ResourceQuery();
+        }
+    }
+}
