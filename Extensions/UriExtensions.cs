31c5ff469e4e4570868555908c63e120a05426ee
Keith Holloway <keith@eastfive.com>
2016-10-08 15:28:12 -0500

    Added UriExtensions

---
+++ Extensions/UriExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+
+namespace BlackBarLabs.Web
+{
+    public static class UriExtensions
+    {
+        public static Uri AddQueryParameter(this Uri uri, string parameter, string value)
+        {
+            var uriBuilder = new UriBuilder(uri);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query[parameter] = value;
+            uriBuilder.Query = query.ToString();
+            return uriBuilder.Uri;
+        }
+    }
+}
