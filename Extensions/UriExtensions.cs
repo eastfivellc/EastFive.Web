1a9ff65bd941892b6cc0b145ac472be10b463f0c
Keith Holloway <keith@eastfive.com>
2016-11-03 14:48:27 -0500

    Added Uri.ReplaceQueryParameterValue extension

--- Extensions/UriExtensions.cs
+++ Extensions/UriExtensions.cs
@@ -26,5 +26,14 @@ namespace BlackBarLabs.Web
                 index = parameters.Select(param => Convert.ToInt32(param.Substring(parameter.Length + 1, 1))).Max() + 1;
             return index;
         }
+
+        public static Uri ReplaceQueryParameterValue(this Uri uri, string parameterName, string newValue)
+        {
+            var uriBuilder = new UriBuilder(uri);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query.Set(parameterName, newValue);
+            uriBuilder.Query = query.ToString();
+            return uriBuilder.Uri;
+        }
     }
 }
