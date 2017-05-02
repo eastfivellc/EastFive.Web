f120163970ee319a5668eec3c582a38f54e02397
Josh <josh@eastfive.com>
2017-05-02 13:20:56 -0500

    extension method for checking if HttpStatusCode is success

--- Extensions/WebRequestExtensions.cs
+++ Extensions/WebRequestExtensions.cs
@@ -178,5 +178,9 @@ namespace BlackBarLabs.Web
 
         #endregion
 
+        public static bool IsSuccess(this HttpStatusCode statusCode)
+        {
+            return ((int)statusCode >= 200) && ((int)statusCode <= 299);
+        }
     }
 }
