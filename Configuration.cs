96fe37ad1b3e4a8598ad2cd4548837bfc9256f60
Josh <josh@eastfive.com>
2017-06-03 14:23:56 -0500

    added config lookups that are TResult for error catching

--- Configuration.cs
+++ Configuration.cs
@@ -13,9 +13,47 @@ namespace EastFive.Web.Configuration
             return Microsoft.Azure.CloudConfigurationManager.GetSetting(key);
         }
 
+        [Obsolete]
         public static Uri GetUri(string key)
         {
             return new Uri(Get(key));
+        }
+
+        public static TResult GetUri<TResult>(string key,
+            Func<Uri, TResult> onParsed,
+            Func<string, TResult> unspecifiedOrInvalid)
+        {
+            var keyValue = Get(key);
+            Uri uriValue;
+            if (!Uri.TryCreate(keyValue, UriKind.RelativeOrAbsolute, out uriValue))
+                return unspecifiedOrInvalid($"The configuration value for [{key}] is not specified. Please specify a URI value");
+            return onParsed(uriValue);
+        }
+
+        public static TResult GetDouble<TResult>(string key,
+            Func<double, TResult> onParsed,
+            Func<string, TResult> unspecifiedOrInvalid)
+        {
+            var keyValue = Get(key);
+            double doubleValue;
+            if (!double.TryParse(keyValue, out doubleValue))
+                return unspecifiedOrInvalid(
+                    $"The configuration value for [{key}] is not specified. Please specify a double value");
+            return onParsed(doubleValue);
+        }
+
+        public static TResult GetString<TResult>(string key,
+            Func<string, TResult> onFound,
+            Func<string, TResult> onUnspecified)
+        {
+            try
+            {
+                var keyValue = Microsoft.Azure.CloudConfigurationManager.GetSetting(key, false, true);
+                return onFound(keyValue);
+            } catch(Exception)
+            {
+                return onUnspecified($"The configuration value for [{key}] is not specified. Please specify a string value");
+            }
         }
     }
 }
