f153480421845b8722111064181272ca770e2978
Josh <josh@eastfive.com>
2017-11-14 18:11:00 -0600

    documenting configs

---
+++ Settings/ConfigAttribute.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Web
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ConfigAttribute : Attribute
+    {
+    }
+
+    public class ConfigKeyAttribute : Attribute
+    {
+        public ConfigKeyAttribute(string description, DeploymentOverrides deploymentOverride)
+        {
+            this.Description = description;
+            this.DeploymentOverride = deploymentOverride;
+        }
+        
+        /// <summary>
+        /// Where to find this value
+        /// </summary>
+        public virtual string Location { get; set; }
+
+        /// <summary>
+        /// What is provided by this value
+        /// </summary>
+        public virtual string Description { get; private set; }
+
+        /// <summary>
+        /// More information about the key.
+        /// </summary>
+        public virtual string MoreInfo { get; set; }
+
+
+        /// <summary>
+        /// Should the value provided in the (web|app).config be overwritten in a deployed environment.
+        /// </summary>
+        public virtual DeploymentOverrides DeploymentOverride { get; }
+        
+        /// <summary>
+        /// Not overriding this value in a deployment is a security concern
+        /// </summary>
+        public virtual bool DeploymentSecurityConcern { get; set; }
+    }
+
+    public enum DeploymentOverrides
+    {
+        /// <summary>
+        /// The system cannot operate correctly without this value being set per environment
+        /// </summary>
+        Mandatory,
+        
+        /// <summary>
+        /// This value should be set per environment 
+        /// </summary>
+        Suggested,
+
+        /// <summary>
+        /// It is recommended to set this value per environment be the default will function
+        /// </summary>
+        Desireable,
+
+        /// <summary>
+        /// This value's default parameters will work for any environment and are only made
+        /// available for configuration for optional tuning.
+        /// </summary>
+        Optional,
+
+        /// <summary>
+        /// This value should not be modified.
+        /// </summary>
+        Forbidden,
+    }
+}
--- Settings/Constants.cs
+++ Settings/Constants.cs
@@ -1,12 +1,23 @@
 ﻿
 namespace EastFive.Web.Configuration
 {
+    [Config]
     public class Constants
     {
+        [Config]
         public static class KeyVault
         {
+            [ConfigKeyAttribute("Location of the KeyVault", DeploymentOverrides.Suggested,
+                Location ="Azure Portal -> KeyValue -> Settings -> (I'm making this up)",
+                DeploymentSecurityConcern = true)]
             public const string Url = "KeyVault.Url";
+            [ConfigKeyAttribute("The KeyValue at the url is multitennant so the client id specifies the tennant",
+                DeploymentOverrides.Suggested,
+                DeploymentSecurityConcern = true)]
             public const string ClientId = "KeyVault.ClientId";
+            [ConfigKeyAttribute("Used by the application to authenticate to the KeyVault",
+                DeploymentOverrides.Suggested,
+                DeploymentSecurityConcern = true)]
             public const string ClientSecret = "KeyVault.ClientSecret";
         }
     }
