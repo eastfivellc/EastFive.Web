bbc203d27f9fd2a86fb38e4078a0847b90bb13fe
Josh Wingstrom <josh@eastfive.com>
2018-02-07 13:45:32 -0600

    added extensions for parsing newtonsoft json results

---
+++ Extensions/JsonExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Web
+{
+    public static class JsonExtensions
+    {
+        public static IEnumerable<dynamic> AsEnumerableDynamics(this JArray array)
+        {
+            foreach (var item in array)
+                yield return (dynamic)item;
+        }
+    }
+}
