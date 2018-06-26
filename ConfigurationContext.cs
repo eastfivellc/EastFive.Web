44077953bf5b0f45f428fc58a4891ea92218d5b8
Scott Turner <turner-scott@tds.net>
2018-06-26 02:28:39 -0500

    get more than 25 keys out of KeyVault and block callers until everything is initialized

--- ConfigurationContext.cs
+++ ConfigurationContext.cs
@@ -19,6 +19,7 @@ namespace BlackBarLabs.Web
         private static volatile ConfigurationContext instance;
         private static object syncRoot = new Object();
         private static Dictionary<string, string> appSettings;
+        private static object syncAppSettings = new Object();
 
         private ConfigurationContext() { }
 
@@ -41,22 +42,43 @@ namespace BlackBarLabs.Web
         private void Initialize()
         {
             var appSettingsFromConfigFile = System.Configuration.ConfigurationManager.AppSettings;
-            appSettings = appSettingsFromConfigFile.AllKeys.Select(
+            var settings = appSettingsFromConfigFile.AllKeys.Select(
                 key =>
                 {
                     return key.PairWithValue(appSettingsFromConfigFile[key]);
                 }).ToDictionary();
 
+            // Set appSettings at the last moment so that the lock keeps everybody out until all is initialized 
+            if (!settings.TryGetValue(EastFive.Web.AppSettings.KeyVault.Url, out string vaultUrl) ||
+                !settings.TryGetValue(EastFive.Web.AppSettings.KeyVault.ClientId, out string clientId) ||
+                !settings.TryGetValue(EastFive.Web.AppSettings.KeyVault.ClientSecret, out string clientSecret))
+            {
+                appSettings = settings;
+                return;
+            }
+
             // Merge Key Vault keys into app settings dictionary.  If there is a key conflict, favor Key Vault.
-            Task.Run(()=> GetKeyVaultSecretsAsync(
+            Task.Run(()=> GetKeyVaultSecretsAsync(vaultUrl, clientId, clientSecret,
                 keyVaultValues =>
                 {
-                    appSettings = appSettings.Where(appSetting => !keyVaultValues.Keys.Contains(appSetting.Key)).Concat(keyVaultValues).ToDictionary();
+                    appSettings = settings.Where(pair => !keyVaultValues.Keys.Contains(pair.Key)).Concat(keyVaultValues).ToDictionary();
                     return true;
                 },
-                ()=>false,
-                ()=>false,
-                ()=>false)).GetAwaiter().GetResult();
+                ()=> 
+                {
+                    appSettings = settings;
+                    return false;
+                },
+                () =>
+                {
+                    appSettings = settings;
+                    return false;
+                },
+                () =>
+                {
+                    appSettings = settings;
+                    return false;
+                })).GetAwaiter().GetResult();
         }
 
         public Dictionary<string, string> AppSettings
@@ -64,7 +86,13 @@ namespace BlackBarLabs.Web
             get
             {
                 if (appSettings == null)
-                    Initialize();
+                {
+                    lock (syncAppSettings)
+                    {
+                        if (appSettings == null)
+                            Initialize();
+                    }
+                }
                 return appSettings;
             }
         }
@@ -73,9 +101,6 @@ namespace BlackBarLabs.Web
             Func<string, TResult> onFound,
             Func<TResult> onKeyDoesNotExist)
         {
-            if (appSettings == null)
-                Initialize();
-
             if (!AppSettings.ContainsKey(key))
                 return onKeyDoesNotExist();
 
@@ -84,59 +109,48 @@ namespace BlackBarLabs.Web
 
         #region Key Vault Support
 
-        private async static Task<TResult> GetKeyVaultSecretsAsync<TResult>(
+        private async static Task<TResult> GetKeyVaultSecretsAsync<TResult>(string vaultUrl, string clientId, string clientSecret,
              Func<Dictionary<string, string>, TResult> onFound,
              Func<TResult> onNotFound,
              Func<TResult> onKeyVaultTokenInvalid,
              Func<TResult> onKeyVaultNotConfigured)
         {
+            if (string.IsNullOrEmpty(vaultUrl) ||
+                string.IsNullOrEmpty(clientId) ||
+                string.IsNullOrEmpty(clientSecret))
+                return onKeyVaultNotConfigured();
+
             try
             {
-                var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetTokenAsync));
-                var vaultUrl = EastFive.Web.Configuration.Settings.GetString(EastFive.Web.AppSettings.KeyVault.Url, value => value, (reason) => string.Empty);
-                if (string.IsNullOrEmpty(vaultUrl))
-                    return onKeyVaultNotConfigured();
+                var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(
+                    async (authority, resource, scope) => await GetTokenAsync(clientId, clientSecret, authority, resource, scope)));
 
                 var secretBundle = await keyVaultClient.GetSecretsAsync(vaultUrl);
                 if (null == secretBundle)
                     return onNotFound();
 
-                var secrets = await secretBundle.Select(
-                    async secret =>
-                    {
-                        var secretValue = await GetKeyVaultSecretAsync(secret.Identifier.Name,
-                            value => value,
-                            () => string.Empty,
-                            () => string.Empty);
+                // The api returns 25 keys at a time
+                var names = secretBundle.Select(secret => secret.Identifier.Name).ToArray();
+                while (!string.IsNullOrEmpty(secretBundle.NextPageLink))
+                {
+                    secretBundle = await keyVaultClient.GetSecretsNextAsync(secretBundle.NextPageLink);
+                    if (secretBundle == null)
+                        break;
+                    names = names.Concat(secretBundle.Select(secret => secret.Identifier.Name)).ToArray();
+                };
 
-                        // KeyVault will only allow Alphanumberic characters and dashes.  Replace - with . to keep our  
-                        // current naming convention.  Yes - this means we cannot have dashes in our names.
-                        return secret.Identifier.Name.Replace("-", ".").PairWithValue(secretValue);
-                    }).WhenAllAsync();
+                var secrets = await names
+                    .Select(
+                        async name =>
+                        {
+                            var secret = await keyVaultClient.GetSecretAsync(vaultUrl, name);
+                            var value = (null == secret) ? string.Empty : secret.Value;
+                            // KeyVault will only allow Alphanumberic characters and dashes.  Replace - with . to keep our  
+                            // current naming convention.  Yes - this means we cannot have dashes in our names.
+                            return name.Replace("-", ".").PairWithValue(value);
+                        }).WhenAllAsync(1);
 
                 return onFound(secrets.ToDictionary());
-            }
-            catch (Exception e)
-            {
-                return onKeyVaultTokenInvalid();
-            }
-        }
-
-        private async static Task<TResult> GetKeyVaultSecretAsync<TResult>(string key,
-             Func<string, TResult> onFound,
-             Func<TResult> onNotFound,
-             Func<TResult> onKeyVaultTokenInvalid)
-        {
-            try
-            {
-                var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetTokenAsync));
-                var vaultUrl = appSettings[EastFive.Web.AppSettings.KeyVault.Url];
-
-                var secretBundle = await keyVaultClient.GetSecretAsync(vaultUrl, key);
-                if (null == secretBundle)
-                    return onNotFound();
-
-                return onFound(secretBundle.Value);
             }
             catch (Exception e)
             {
@@ -151,11 +165,8 @@ namespace BlackBarLabs.Web
         /// <param name="resource"></param>
         /// <param name="scope"></param>
         /// <returns></returns>
-        private static async Task<string> GetTokenAsync(string authority, string resource, string scope)
+        private static async Task<string> GetTokenAsync(string clientId, string clientSecret, string authority, string resource, string scope)
         {
-            var clientId = appSettings[EastFive.Web.AppSettings.KeyVault.ClientId];
-            var clientSecret = appSettings[EastFive.Web.AppSettings.KeyVault.ClientSecret];
-
             var authContext = new AuthenticationContext(authority);
             var clientCred = new ClientCredential(clientId, clientSecret);
             var result = await authContext.AcquireTokenAsync(resource, clientCred);
