706e23ada5736c0af6b6d5cb964e7359390a95bf
Joshua Wingstrom <josh@eastfive.com>
2019-06-17 07:40:17 -0500

    moved api exceptions to .API project

---
+++ Exceptions/ConfigurationException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastFive.Web
+{
+    public class ConfigurationException : Exception
+    {
+
+        public string ParameterName { get; private set; }
+
+        public Type ParameterType { get; private set; }
+
+        public Type ResultType { get; private set; }
+
+        public ConfigurationException(string parameterName, Type parameterType, string message, Type resultType)
+            : base(message)
+        {
+            this.ParameterName = parameterName;
+            this.ParameterType = parameterType;
+            this.ResultType = resultType;
+        }
+
+        public static TResult OnConfigurationFailure<TResult>(string parameterName, Type parameterType, string message)
+        {
+            throw new ConfigurationException(parameterName, parameterType, message, typeof(TResult));
+        }
+
+        public static TResult OnConfigurationFailureWhy<TResult>(string message)
+        {
+            throw new ConfigurationException(string.Empty, typeof(string), message, typeof(TResult));
+        }
+    }
+}
