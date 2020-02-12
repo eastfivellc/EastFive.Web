701230bc9b66931084865067d7099f74834c4744
Joshua Wingstrom <josh@eastfive.com>
2020-02-11 19:42:03 -0600

    claims rework

--- Tokens/ClaimIds.cs
+++ Tokens/ClaimIds.cs
@@ -1,56 +1,56 @@
-﻿using System;
-using System.Linq;
-using System.Security.Claims;
+﻿//using System;
+//using System.Linq;
+//using System.Security.Claims;
 
-namespace BlackBarLabs.Security
-{
-    public static class ClaimIds
-    {
-        public const string Session = "session";
-        public const string Authorization = "authorization";
-        public const string Role = "role";
+//namespace BlackBarLabs.Security
+//{
+//    public static class ClaimIds
+//    {
+//        public const string Session = "session";
+//        public const string Authorization = "authorization";
+//        public const string Role = "role";
 
-        public static ClaimsIdentity GenerateClaimsIdentity(Guid sessionId, Guid authorizationId)
-        {
-            var userClaims = new ClaimsIdentity();
-            userClaims.AddClaim(new Claim(Session, sessionId.ToString()));
-            userClaims.AddClaim(new Claim(Authorization, authorizationId.ToString()));
-            return userClaims;
-        }
-        public static ClaimsIdentity GenerateClaimsIdentity(Guid sessionId, Guid authorizationId, int role)
-        {
-            var userClaims = new ClaimsIdentity();
-            userClaims.AddClaim(new Claim(Session, sessionId.ToString()));
-            userClaims.AddClaim(new Claim(Authorization, authorizationId.ToString()));
-            userClaims.AddClaim(new Claim(Role, role.ToString()));
-            return userClaims;
-        }
+//        public static ClaimsIdentity GenerateClaimsIdentity(Guid sessionId, Guid authorizationId)
+//        {
+//            var userClaims = new ClaimsIdentity();
+//            userClaims.AddClaim(new Claim(Session, sessionId.ToString()));
+//            userClaims.AddClaim(new Claim(Authorization, authorizationId.ToString()));
+//            return userClaims;
+//        }
+//        public static ClaimsIdentity GenerateClaimsIdentity(Guid sessionId, Guid authorizationId, int role)
+//        {
+//            var userClaims = new ClaimsIdentity();
+//            userClaims.AddClaim(new Claim(Session, sessionId.ToString()));
+//            userClaims.AddClaim(new Claim(Authorization, authorizationId.ToString()));
+//            userClaims.AddClaim(new Claim(Role, role.ToString()));
+//            return userClaims;
+//        }
 
 
-        public static bool TryGetAuthorization(this System.Security.Principal.IIdentity identity,
-            out Guid sessionId, out Guid authorizationId)
-        {
-            sessionId = Guid.Empty;
-            authorizationId = Guid.Empty;
+//        public static bool TryGetAuthorization(this System.Security.Principal.IIdentity identity,
+//            out Guid sessionId, out Guid authorizationId)
+//        {
+//            sessionId = Guid.Empty;
+//            authorizationId = Guid.Empty;
 
-            var claimsIdentity = identity as ClaimsIdentity;
-            if (claimsIdentity == null)
-                return false;
+//            var claimsIdentity = identity as ClaimsIdentity;
+//            if (claimsIdentity == null)
+//                return false;
 
-            var claims = claimsIdentity.Claims.ToList();
+//            var claims = claimsIdentity.Claims.ToList();
 
-            var authorizationClaim = claims.FirstOrDefault(x => x.Type == ClaimIds.Authorization);
-            if (default(Claim) == authorizationClaim)
-                return false;
-            var sessionClaim = claims.FirstOrDefault(x => x.Type == ClaimIds.Session);
-            if (default(Claim) == sessionClaim)
-                return false;
+//            var authorizationClaim = claims.FirstOrDefault(x => x.Type == ClaimIds.Authorization);
+//            if (default(Claim) == authorizationClaim)
+//                return false;
+//            var sessionClaim = claims.FirstOrDefault(x => x.Type == ClaimIds.Session);
+//            if (default(Claim) == sessionClaim)
+//                return false;
 
-            authorizationId = Guid.Parse(authorizationClaim.Value);
-            sessionId = Guid.Parse(sessionClaim.Value);
+//            authorizationId = Guid.Parse(authorizationClaim.Value);
+//            sessionId = Guid.Parse(sessionClaim.Value);
 
-            return !(sessionId == Guid.Empty || authorizationId == Guid.Empty);
-        }
+//            return !(sessionId == Guid.Empty || authorizationId == Guid.Empty);
+//        }
 
-    }
-}
+//    }
+//}
--- Tokens/JwtTools.cs
+++ Tokens/JwtTools.cs
@@ -13,23 +13,23 @@ namespace BlackBarLabs.Security.Tokens
 {
     public static class JwtTools
     {
-        public static TResult GetSessionId<TResult>(this Claim[] claims,
-            Func<Guid, TResult> found,
-            Func<TResult> notFound = default(Func<TResult>),
-            Func<TResult> invalid = default(Func<TResult>))
-        {
-            return claims.GetGuidValue(ClaimIds.Session,
-                found, notFound, invalid);
-        }
+        //public static TResult GetSessionId<TResult>(this Claim[] claims,
+        //    Func<Guid, TResult> found,
+        //    Func<TResult> notFound = default(Func<TResult>),
+        //    Func<TResult> invalid = default(Func<TResult>))
+        //{
+        //    return claims.GetGuidValue(ClaimIds.Session,
+        //        found, notFound, invalid);
+        //}
 
-        public static TResult GetAuthId<TResult>(this Claim[] claims,
-            Func<Guid, TResult> found,
-            Func<TResult> notFound = default(Func<TResult>),
-            Func<TResult> invalid = default(Func<TResult>))
-        {
-            return claims.GetGuidValue(ClaimIds.Authorization,
-                found, notFound, invalid);
-        }
+        //public static TResult GetAuthId<TResult>(this Claim[] claims,
+        //    Func<Guid, TResult> found,
+        //    Func<TResult> notFound = default(Func<TResult>),
+        //    Func<TResult> invalid = default(Func<TResult>))
+        //{
+        //    return claims.GetGuidValue(ClaimIds.Authorization,
+        //        found, notFound, invalid);
+        //}
 
         public static TResult GetGuidValue<TResult>(this Claim[] claims, string key,
             Func<Guid, TResult> found,
@@ -121,90 +121,6 @@ namespace BlackBarLabs.Security.Tokens
             return result;
         }
 
-        public static TResult CreateToken<TResult>(Guid sessionId, Uri scope,
-            TimeSpan duration,
-            Func<string, TResult> tokenCreated,
-            Func<string, TResult> missingConfigurationSetting,
-            Func<string, string, TResult> invalidConfigurationSetting,
-            string configNameOfIssuer = EastFive.Security.AppSettings.TokenIssuer,
-            string configNameOfRSAKey = EastFive.Security.AppSettings.TokenKey)
-        {
-            return CreateToken(sessionId, scope, duration, default(IDictionary<string, string>),
-                tokenCreated, missingConfigurationSetting, invalidConfigurationSetting,
-                configNameOfIssuer, configNameOfRSAKey);
-        }
-
-        public static TResult CreateToken<TResult>(Guid sessionId,
-                Uri scope, TimeSpan duration, IDictionary<string, string> claims,
-            Func<string, TResult> tokenCreated,
-            Func<string, TResult> missingConfigurationSetting,
-            Func<string, string, TResult> invalidConfigurationSetting,
-            string configNameOfIssuer = EastFive.Security.AppSettings.TokenIssuer,
-            string configNameOfRSAKey = EastFive.Security.AppSettings.TokenKey)
-        {
-            var claimsAuth = (IEnumerable<Claim>)new[]
-            {
-                new Claim(ClaimIds.Session, sessionId.ToString()),
-            };
-            var claimsCrypt = claims.NullToEmpty().Select(kvp => new Claim(kvp.Key, kvp.Value));
-
-            var issued = DateTime.UtcNow;
-            var result = CreateToken(scope,
-                issued, duration, claimsAuth.Concat(claimsCrypt),
-                tokenCreated, missingConfigurationSetting, invalidConfigurationSetting,
-                configNameOfIssuer, configNameOfRSAKey);
-            return result;
-        }
-
-        public static TResult CreateToken<TResult>(Guid sessionId, Guid authId, Uri scope,
-            TimeSpan duration,
-            Func<string, TResult> tokenCreated,
-            Func<string, TResult> missingConfigurationSetting,
-            Func<string, string, TResult> invalidConfigurationSetting,
-            string configNameOfIssuer = EastFive.Security.AppSettings.TokenIssuer,
-            string configNameOfRSAKey = EastFive.Security.AppSettings.TokenKey)
-        {
-            return CreateToken(sessionId, authId, scope, duration, default(IDictionary<string, string>),
-                tokenCreated, missingConfigurationSetting, invalidConfigurationSetting,
-                configNameOfIssuer, configNameOfRSAKey);
-        }
-
-        public static TResult CreateToken<TResult>(Guid sessionId, Guid authId, Uri scope,
-            TimeSpan duration,
-            IDictionary<string, string> claims,
-            Func<string, TResult> tokenCreated,
-            Func<string, TResult> missingConfigurationSetting,
-            Func<string, string, TResult> invalidConfigurationSetting,
-            string configNameOfIssuer = EastFive.Security.AppSettings.TokenIssuer,
-            string configNameOfRSAKey = EastFive.Security.AppSettings.TokenKey)
-        {
-            var claimsCrypt = claims.NullToEmpty().Select(kvp => new Claim(kvp.Key, kvp.Value));
-            return CreateToken(sessionId, authId, scope, duration, claimsCrypt, tokenCreated, missingConfigurationSetting, invalidConfigurationSetting,
-                configNameOfIssuer, configNameOfRSAKey);
-        }
-
-        public static TResult CreateToken<TResult>(Guid sessionId, Guid authId, Uri scope,
-            TimeSpan duration,
-            IEnumerable<Claim> claims,
-            Func<string, TResult> tokenCreated,
-            Func<string, TResult> missingConfigurationSetting,
-            Func<string, string, TResult> invalidConfigurationSetting,
-            string configNameOfIssuer = EastFive.Security.AppSettings.TokenIssuer,
-            string configNameOfRSAKey = EastFive.Security.AppSettings.TokenKey)
-        {
-            var claimsAuth = new[] {
-                new Claim(ClaimIds.Session, sessionId.ToString()),
-                new Claim(ClaimIds.Authorization, authId.ToString()) };
-            var claimsCrypt = claims.NullToEmpty();
-
-            var issued = DateTime.UtcNow;
-            var result = CreateToken(scope,
-                issued, duration, claimsAuth.Concat(claimsCrypt),
-                tokenCreated, missingConfigurationSetting, invalidConfigurationSetting,
-                configNameOfIssuer, configNameOfRSAKey);
-            return result;
-        }
-
         public static TResult CreateToken<TResult>(Uri scope,
             DateTime issued, TimeSpan duration,
             IEnumerable<Claim> claims,
@@ -240,14 +156,6 @@ namespace BlackBarLabs.Security.Tokens
             return String.Compare(strA, strB, ignoreCase) == 0;
         }
 
-        private static IEnumerable<TSource> NullToEmpty<TSource>(
-            this IEnumerable<TSource> source)
-        {
-            if (default(IEnumerable<TSource>) == source)
-                return new TSource[] { };
-            return source;
-        }
-        
         public static TResult FirstOrDefault<T, TResult>(this IEnumerable<T> items,
             Func<T, TResult> found,
             Func<TResult> notFound)
