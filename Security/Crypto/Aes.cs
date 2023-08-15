4a102a3d0b4be242a623e8279d9d1ae434e91bea
Scott Turner <scott@eastfive.com>
2023-08-15 07:28:52 -0500

    AES helpers

---
+++ Security/Crypto/Aes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace EastFive.Security.Crypto
+{
+	public static class Aes
+	{
+		public static byte[] AesEncrypt(this byte[] dataToEncrypt, System.Security.Cryptography.Aes provider)
+		{
+			var encryptor = provider.CreateEncryptor();
+            using (var ms = new MemoryStream())
+            using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+            using (var sw = new StreamWriter(cs))
+            {
+                sw.Write(dataToEncrypt);
+                sw.Close();
+                return ms.ToArray();
+            }
+        }
+
+        public static byte[] AesDecrypt(this byte[] dataToDecrypt, System.Security.Cryptography.Aes provider)
+        {
+            var decryptor = provider.CreateDecryptor();
+            using (var ms = new MemoryStream(dataToDecrypt))
+            using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+            {
+                return ms.ToArray();
+            }
+        }
+
+        public static Guid AesEncrypt(this Guid dataToEncrypt, System.Security.Cryptography.Aes provider)
+        {
+            var result = AesEncrypt(dataToEncrypt.ToByteArray(), provider);
+            return new Guid(result);
+        }
+
+        public static Guid AesDecrypt(this Guid dataToDecrypt, System.Security.Cryptography.Aes provider)
+        {
+            var result = AesDecrypt(dataToDecrypt.ToByteArray(), provider);
+            return new Guid(result);
+        }
+    }
+}
---
+++ Security/Utilities/Aes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace EastFive.Security
+{
+	public static class Aes
+	{
+        public static TResult Generate<TResult>(
+            Func<string, // key
+                string,  // initialization vector (IV)
+                TResult> onSuccess,
+                int keySize = 128)
+        {
+            using (var provider = System.Security.Cryptography.Aes.Create())
+            {
+                if (provider.KeySize != keySize)
+                    provider.KeySize = keySize;
+
+                return onSuccess(
+                    Convert.ToBase64String(provider.Key),
+                    Convert.ToBase64String(provider.IV));
+            }
+        }
+
+        public static TResult FromBase64Strings<TResult>(string secretAseKeyBase64, string secretAseInitializationVectorBase64,
+            Func<System.Security.Cryptography.Aes, TResult> onSuccess,
+            Func<string, TResult> invalidToken)
+        {
+            var provider = System.Security.Cryptography.Aes.Create();
+            try
+            {
+                provider.Key = Convert.FromBase64String(secretAseKeyBase64);
+                provider.IV = Convert.FromBase64String(secretAseInitializationVectorBase64);
+                return onSuccess(provider);
+            }
+            catch (CryptographicException ex)
+            {
+                return invalidToken(ex.Message);
+            }
+        }
+    }
+}
+
