697ec7245a80c46583bf8afad9aea84f2f5eef00
Scott Turner <scott@eastfive.com>
2023-08-15 08:00:44 -0500

    provide AES secrets as GUIDs

--- Security/Utilities/Aes.cs
+++ Security/Utilities/Aes.cs
@@ -9,7 +9,7 @@ namespace EastFive.Security
             Func<string, // key
                 string,  // initialization vector (IV)
                 TResult> onSuccess,
-                int keySize = 128)
+                int keySize = 256)
         {
             using (var provider = System.Security.Cryptography.Aes.Create())
             {
@@ -22,6 +22,20 @@ namespace EastFive.Security
             }
         }
 
+        public static TResult Generate128<TResult>(
+            Func<Guid, // key
+                Guid,  // initialization vector (IV)
+                TResult> onSuccess)
+        {
+            using (var provider = System.Security.Cryptography.Aes.Create())
+            {
+                provider.KeySize = 128;
+                return onSuccess(
+                    new Guid(provider.Key),
+                    new Guid(provider.IV));
+            }
+        }
+
         public static TResult FromBase64Strings<TResult>(string secretAseKeyBase64, string secretAseInitializationVectorBase64,
             Func<System.Security.Cryptography.Aes, TResult> onSuccess,
             Func<string, TResult> invalidToken)
@@ -31,6 +45,23 @@ namespace EastFive.Security
             {
                 provider.Key = Convert.FromBase64String(secretAseKeyBase64);
                 provider.IV = Convert.FromBase64String(secretAseInitializationVectorBase64);
+                return onSuccess(provider);
+            }
+            catch (CryptographicException ex)
+            {
+                return invalidToken(ex.Message);
+            }
+        }
+
+        public static TResult FromGuids<TResult>(Guid secretAseKey, Guid secretAseInitializationVector,
+            Func<System.Security.Cryptography.Aes, TResult> onSuccess,
+            Func<string, TResult> invalidToken)
+        {
+            var provider = System.Security.Cryptography.Aes.Create();
+            try
+            {
+                provider.Key = secretAseKey.ToByteArray();
+                provider.IV = secretAseInitializationVector.ToByteArray();
                 return onSuccess(provider);
             }
             catch (CryptographicException ex)
