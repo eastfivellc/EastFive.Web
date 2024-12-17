b6edf7654982cc2f2e742e560e37b6585f06cb26
Scott Turner <scott@eastfive.com>
2024-12-16 19:33:22 -0600

    use AES for securing longer text

--- Security/Crypto/Aes.cs
+++ Security/Crypto/Aes.cs
@@ -1,23 +1,13 @@
 using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace EastFive.Security.Crypto
 {
 	public static class Aes
 	{
-        public static TResult GenerateGuidKey<TResult>(
-            Func<Guid, // key
-                TResult> onSuccess)
-        {
-            using (var provider = System.Security.Cryptography.Aes.Create())
-            {
-                provider.KeySize = 128; // other key sizes won't fit in a GUID
-                return onSuccess(
-                    new Guid(provider.Key)); 
-            }
-        }
-
+        #region GUID data
         public static Guid AesEncrypt(this Guid dataToEncrypt, Guid secretAseKey)
         {
             using (var aes = System.Security.Cryptography.Aes.Create())
@@ -71,5 +61,48 @@ namespace EastFive.Security.Crypto
                 }
             }
         }
+        #endregion GUID data
+
+        #region string data
+        // as needed, call aes.GenerateIV() before calling this
+        public static (byte[] encrypted, byte[] iv) AesEncrypt(this byte[] bytes, System.Security.Cryptography.Aes aes)
+        {
+            var iv = aes.IV;
+            using (var e = aes.CreateEncryptor())
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, e, CryptoStreamMode.Write))
+                {
+                    cs.Write(bytes, 0, bytes.Length);
+                    cs.FlushFinalBlock();
+                }
+                return (ms.ToArray(), iv);
+            }
+        }
+
+        // call aes.IV = stuff before calling this
+        public static byte[] AesDecrypt(this byte[] encrypted, System.Security.Cryptography.Aes aes)
+        {
+            using (var d = aes.CreateDecryptor())
+            using (var ms = new MemoryStream(encrypted))
+            using (var cs = new CryptoStream(ms, d, CryptoStreamMode.Read))
+            {
+                var output = new byte[encrypted.Length];
+
+                // using .NET 6 approach recommended here: https://github.com/dotnet/runtime/issues/61398
+                int num = 0;
+                while (num < output.Length)
+                {
+                    int bytesRead = cs.Read(output, num, output.Length - num);
+                    if (bytesRead == 0)
+                        break;
+                    num += bytesRead;
+                }
+                var trimmed = new byte[num];
+                Array.Copy(output, trimmed, num);
+                return trimmed;
+            }
+        }
+        #endregion string data
     }
 }
---
+++ Security/Utilities/AES.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using EastFive.Web.Configuration;
+
+namespace EastFive.Security
+{
+	public static class AES
+	{
+        public static TResult AESFromConfig<TResult>(string keyConfigurationName, string keySizeConfigurationName,
+            Func<System.Security.Cryptography.Aes, TResult> onSuccess,
+            Func<TResult> onNotSpecified,
+            Func<string, TResult> onFailure)
+        {
+            return keyConfigurationName.ConfigurationBase64Bytes(
+                (keyBytes) => keySizeConfigurationName.ConfigurationLong(
+                    (keySize) =>
+                    {
+                        var aes = System.Security.Cryptography.Aes.Create();
+                        aes.Padding = PaddingMode.ISO10126;
+                        aes.KeySize = (int)keySize;
+                        aes.Key = keyBytes;
+                        return onSuccess(aes);
+                    },
+                    onFailure,
+                    onNotSpecified),
+                onFailure,
+                onNotSpecified);
+        }
+
+        public static TResult GenerateGuidKey<TResult>(
+            Func<Guid, // key
+                TResult> onSuccess)
+        {
+            using (var provider = System.Security.Cryptography.Aes.Create())
+            {
+                provider.KeySize = 128; // other key sizes won't fit in a GUID
+                return onSuccess(
+                    new Guid(provider.Key));
+            }
+        }
+
+        public static TResult GenerateKey<TResult>(
+        Func<string, // key
+            TResult> onSuccess,
+            int keySize = 256)
+        {
+            using (var provider = System.Security.Cryptography.Aes.Create())
+            {
+                provider.KeySize = keySize;
+                return onSuccess(
+                    Convert.ToBase64String(provider.Key));
+            }
+        }
+    }
+}
+
